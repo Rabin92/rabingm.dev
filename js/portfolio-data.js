ad02f3f4857488fa09a4a8e4ddad2d9feec57e30
Rabin Gharti Magar <rabin_mgr@live.com>
2020-11-10 13:12:21 +0000

    file modified

--- js/portfolio-data.js
+++ js/portfolio-data.js
@@ -1,6 +1,6 @@
 const projects = [
   {
-    photo_1: 'dashboard.png',
+    image: 'dashboard.png',
     alt_text: 'dashboard',
     live_link: 'https://rabin92.github.io/techdegree-project7/',
     code_link: 'https://github.com/Rabin92/techdegree-project7',
@@ -13,7 +13,7 @@ const projects = [
     `,
   },
   {
-    photo_1: 'employee-dir.png',
+    image: 'employee-dir.png',
     alt_text: 'employee-directory',
     live_link: 'https://rabin92.github.io/techdegree-project8/',
     code_link: 'https://github.com/Rabin92/techdegree-project8',
@@ -25,7 +25,7 @@ const projects = [
     `,
   },
   {
-    photo_1: 'guess_game.png',
+    image: 'guess_game.png',
     alt_text: 'guessing-game',
     live_link: 'https://rabin92.github.io/techdegree-project6/',
     code_link: 'https://github.com/Rabin92/techdegree-project6',
@@ -37,7 +37,7 @@ const projects = [
     `,
   },
   {
-    photo_1: 'photo_gallery.png',
+    image: 'photo_gallery.png',
     alt_text: 'photo-gallery',
     live_link: 'https://rabin92.github.io/techdegree-project5/',
     code_link: 'https://github.com/Rabin92/techdegree-project5',
@@ -48,7 +48,7 @@ const projects = [
     `,
   },
   {
-    photo_1: 'form.png',
+    image: 'form.png',
     alt_text: 'Form',
     live_link: 'https://rabin92.github.io/techdegree-project3/',
     code_link: 'https://github.com/Rabin92/techdegree-project3',
@@ -61,7 +61,7 @@ const projects = [
     `,
   },
   {
-    photo_1: 'sass.png',
+    image: 'sass.png',
     alt_text: 'Sass',
     live_link: 'https://rabin92.github.io/techdegree-project4/',
     code_link: 'https://github.com/Rabin92/techdegree-project4',
@@ -80,7 +80,7 @@ projects.forEach(project => {
   html += `
 <div class="card">
     <div class="section-content">
-        <img class="img-portfolio" src=images/portfolio_img/${project.photo_1} alt="${project.alt_text}">
+        <img class="img-portfolio" src=images/portfolio_img/${project.image} alt="${project.alt_text}">
         <div class="section-overlay section-overlay-blur">
             <div class="info">
                 <a href="${project.live_link}"
