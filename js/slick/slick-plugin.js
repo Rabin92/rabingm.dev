f62972a5a238b55a1972a8c0a61e1c5e973d0d45
Rabin Gharti Magar <rabin_mgr@live.com>
2020-12-09 16:38:34 +0000

    plugin settings modified

--- js/slick/slick-plugin.js
+++ js/slick/slick-plugin.js
@@ -6,29 +6,34 @@ $('.slides').slick({
   dots: true,
   autoplay: true,
   autoplaySpeed: 3000,
+  centerMode: true,
 
   responsive: [
     {
-      breakpoint: 1024,
-      settings: {
-        slidesToShow: 2,
-        slidesToScroll: 3,
-        infinite: true,
-        dots: true,
-      },
-    },
-    {
-      breakpoint: 600,
+      breakpoint: 1230,
       settings: {
         slidesToShow: 2,
         slidesToScroll: 2,
+        infinite: true,
+        dots: true,
+        centerMode: false,
       },
     },
+
+    {
+      breakpoint: 680,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+      },
+    },
+
     {
       breakpoint: 480,
       settings: {
         slidesToShow: 1,
         slidesToScroll: 1,
+        centerMode: false,
       },
     },
   ],
